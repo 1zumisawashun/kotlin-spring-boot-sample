42dd7fae70f05562a80b0218a35e21f7781c827b
1zumisawashun <shun.1zumisawa@gmail.com>
2025-04-13 23:06:49 +0900

    fix: jooqの向きをcustomerからusersに変更する

--- src/main/kotlin/com/example/kotlin_spring_boot_sample/demo/UserRepository.kt
+++ src/main/kotlin/com/example/kotlin_spring_boot_sample/demo/UserRepository.kt
@@ -1,6 +1,6 @@
 package com.example.kotlin_spring_boot_sample.demo
 
-import com.example.kotlin_spring_boot_sample.demo.jooq.tables.Customer.CUSTOMER
+import com.example.kotlin_spring_boot_sample.demo.jooq.tables.Users.USERS
 import org.jooq.DSLContext
 import org.springframework.stereotype.Repository
 
@@ -8,24 +8,26 @@ import org.springframework.stereotype.Repository
 class UserRepositoryImpl(private val dsl: DSLContext) : UserRepository {
 
     override fun find(): List<User> {
-        val record = dsl.selectFrom(CUSTOMER).fetch()
-        return record.map { user -> User(id = user.id, name = user.firstName) }
+        val record = dsl.selectFrom(USERS).fetch()
+        return record.map { user -> User(id = user.id, name = user.name) }
     }
 
     override fun findById(id: Int): User? {
-        val record = dsl.selectFrom(CUSTOMER)
-            .where(CUSTOMER.ID.eq(id))
+        val record = dsl.selectFrom(USERS)
+            .where(USERS.ID.eq(id))
             .fetchOne()
 
         return record?.let {
-            User(it.id, it.firstName)
+            User(it.id, it.name)
         }
     }
 
     override fun save(user: User) {
-        val result = dsl.insertInto(CUSTOMER)
-            .set(CUSTOMER.ID, user.id)
-            .set(CUSTOMER.FIRST_NAME, user.name)
+        val result = dsl.insertInto(USERS)
+            .set(USERS.ID, user.id)
+            .set(USERS.NAME, user.name)
             .execute()
+
+        println(result)
     }
 }
